d220695aa92897fac75bcfffc49db07266403030
wpexia <wpexia@gmail.com>
2015-03-21 15:09:26 +0800

    放弃使用BaseSpider 之前的结果暂存

--- gpj_spider/gpj/spiders/gpj_spider.py
+++ gpj_spider/gpj/spiders/gpj_spider.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 __author__ = 'wpexia'
 
-from scrapy.spider import BaseSpider as Spider
+from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 
 from gpj.items import GpjItem
 
-class gpjSpider(Spider):
+
+class GpjSpider(BaseSpider):
     name = "gpjSpider"
     allowed_domains = ["http://www.xin.com/"]
     start_urls = [
@@ -14,12 +16,35 @@ class gpjSpider(Spider):
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
+        number = int(hxs.select('//div[contains(@class,"con-page search_page_link")]/a[last()-1]/text()').extract()[0])
         items = []
-        cars = hxs.select('//div[contains(@class,"car-vtc")]');
-        for car in cars:
-            item = GpjItem()
-            item['title'] = car.select('div[contains(@class,"vtc-info")]/p/a/text()').extract()
-            item['licenseTime'] = car.select('div[contains(@class,"vtc-info")]/div[contains(@class,"box")]/ul/li[1]/text()').extract()
-
-            items.append(item)
+        for i in range(1, 2):
+            build_url = 'http://www.xin.com/quanguo/s/o2a2i%sv1/' % (i)
+            items.append(self.make_requests_from_url(build_url).replace(callback=self.parse_index))
         return items
+
+    def parse_index(self, response):
+        hxs = HtmlXPathSelector(response)
+        urls = hxs.select('//div[contains(@class,"car-vtc vtc-border")]/div[contains(@class,"vtc-img")]/a/@href')
+        items = []
+        for url in urls:
+            build_url = 'http://www.xin.com/%s' % (url.extract())
+            items.append(self.make_requests_from_url(build_url).replace(callback=self.parse_page))
+        return items
+
+    def parse_page(self, response):
+        hxs = HtmlXPathSelector(response)
+        item = GpjItem()
+        item['licenseTime'] = hxs.select('//ul[contains(@class,"contit")]/li[1]/em/text()').extract()
+        item['mileage'] = hxs.select('//ul[contains(@class,"contit")]/li[2]/em/text()').extract()
+        item['price'] = hxs.select('//div[contains(@class,"wan_1")]/em/text()').extract()
+        item['title'] = hxs.select('//div[contains(@class,"tit")]/h1/text()').extract()
+        item['img'] = hxs.select('//div[contains(@class,"d-photo img-album")]/a/img/@src').extract()
+        item['url'] = response.url
+        item['tags'] = {}
+        importants = hxs.select('//div[contains(@class,"configur")]/ul/li')
+        imp = []
+        for important in importants:
+            imp.append(important.select('em/text()').extract())
+        item['tags']["重要"] = imp
+        return item
