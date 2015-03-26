8a8c429227d831a02859f91f901cf47a875b5215
wpexia <wpexia@gmail.com>
2015-03-26 17:37:06 +0800

    使用FineUI前端框架

--- gpj_spider/gpj/spiders/gpj_spider.py
+++ gpj_spider/gpj/spiders/gpj_spider.py
@@ -29,7 +29,11 @@ class GpjSpider(BaseSpider):
         items = []
         for url in urls:
             build_url = 'http://www.xin.com%s' % (url.extract())
-            items.append(self.make_requests_from_url(build_url).replace(callback=self.parse_page))
+            number = build_url.split("/")[-1].split(".")[0].encode("utf-8")
+            try:
+                item = GpjItem.objects.get(no=number)
+            except GpjItem.DoesNotExist:
+                items.append(self.make_requests_from_url(build_url).replace(callback=self.parse_page))
         return items
 
     def parse_page(self, response):
@@ -39,21 +43,20 @@ class GpjSpider(BaseSpider):
             item = GpjItem.objects.get(no=number)
         except GpjItem.DoesNotExist:
             item = GpjItem()
-        item.licenseTime = hxs.select('//ul[contains(@class,"contit")]/li[1]/em/text()').extract()[0].encode("utf-8")+"-01"
-        item.mileage = hxs.select('//ul[contains(@class,"contit")]/li[2]/em/text()').extract()[0][0:-3].encode("utf-8")
-        item.price = hxs.select('//div[contains(@class,"wan_1")]/em/text()').extract()[0][1:-1].encode("utf-8")
-        item.title = hxs.select('//div[contains(@class,"tit")]/h1/text()').extract()[0].encode("utf-8")
-        item.img = hxs.select('//div[contains(@class,"d-photo img-album")]/a/img/@src').extract()[0].encode("utf-8")
-        item.url = response.url
-        importants = hxs.select('//div[contains(@class,"configur")]/ul/li')
-        imp = []
-        for important in importants:
-            text = important.select('span/text()').extract()
-            if text:
-                imp.append(text[0].encode("utf-8"))
-        item.tags = imp
-        item.transmission = hxs.select('//div[contains(@class,"param clearfix")]/div[2]/table/tr[1]/td[2]/text()').extract()[0].encode("utf-8")
-        item.type = hxs.select('//div[contains(@id,"ulover")]/div[2]/div[2]/table/tr[1]/td[2]/text()').extract()[0].encode("utf-8")
-        item.no = number
-        item.save()
-        return item
+            item.licenseTime = hxs.select('//ul[contains(@class,"contit")]/li[1]/em/text()').extract()[0].encode("utf-8")+"-01"
+            item.mileage = hxs.select('//ul[contains(@class,"contit")]/li[2]/em/text()').extract()[0][0:-3].encode("utf-8")
+            item.price = hxs.select('//div[contains(@class,"wan_1")]/em/text()').extract()[0][1:-1].encode("utf-8")
+            item.title = hxs.select('//div[contains(@class,"tit")]/h1/text()').extract()[0].encode("utf-8")
+            item.img = hxs.select('//div[contains(@class,"d-photo img-album")]/a/img/@src').extract()[0].encode("utf-8")
+            item.url = response.url
+            importants = hxs.select('//div[contains(@class,"configur")]/ul/li')
+            imp = []
+            for important in importants:
+                text = important.select('span/text()').extract()
+                if text:
+                    imp.append(text[0].encode("utf-8"))
+            item.tags = imp
+            item.transmission = hxs.select('//div[contains(@class,"param clearfix")]/div[2]/table/tr[1]/td[2]/text()').extract()[0].encode("utf-8")
+            item.type = hxs.select('//div[contains(@id,"ulover")]/div[2]/div[2]/table/tr[1]/td[2]/text()').extract()[0].encode("utf-8")
+            item.no = number
+            item.save()
